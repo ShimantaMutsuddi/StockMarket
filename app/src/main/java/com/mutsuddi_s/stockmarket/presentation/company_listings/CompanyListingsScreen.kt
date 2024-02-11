3b21355bc67c96f3097fc2f41e0c143ed67a0dc7
Shimanta Mutsuddi <mutsuddi005@gmail.com>
2024-02-11 11:59:47 +0600

    companyItem setup

---
+++ app/src/main/java/com/mutsuddi_s/stockmarket/presentation/company_listings/CompanyListingsScreen.kt
@@ -0,0 +1,14 @@
+package com.mutsuddi_s.stockmarket.presentation.company_listings
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.ComposeNavigator
+
+/*
+@Composable
+
+fun CompanyListingsScreen(
+    navigator: ComposeNavigator.Destination ,//DestinationsNavigator
+    viewmodel: CompanyListingsViewModel = hiltViewModel()
+) {
+}*/
